28eda14f78e1be34b75842661b20a22851b608d5
MutumaMutuma <lewismutuma1000@gmail.com>
2018-08-31 14:32:33 +0300

    added class ProdConfig in config.py

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ class Config:
 
     MOVIE_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&apiKey={}'
 
-class DevConfig:
+class ProdConfig(Config):
+    pass
+class DevConfig(Config):
     DEBUG = True
     pass
